85970ab1c501b646d52aee4a6f5f9d11f0d8d83a
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-25 16:12:34 +1100

    added a draw condition

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ winner = False
 gameWinner = 0
 player = 1 # if player 1: X, player 2: O
 
+# could also put winning indexes in an array and iterate to get winner
 def checkWinner(spots):
     for row in range(3):
         for col in range(3):
@@ -28,6 +29,29 @@ def checkWinner(spots):
                 gameWinner = 2
                 print(f"Winner is Player {gameWinner}!")
                 return True
+            if (row == col and row == 0 and col == 0 and spots[row][col] == "X" and spots[row + 1][col + 1] == "X" and spots[row + 2][col + 2] == "X"): # check diagonal left Player 1
+                gameWinner = 1
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if (row == col and row == 0 and col == 0 and spots[row][col] == "O" and spots[row + 1][col + 1] == "O" and spots[row + 2][col + 2] == "O"): # check diagonal left Player 2
+                gameWinner = 2
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if (row == col and row == 1 and col == 1 and spots[row][col] == "X" and spots[row - 1][col + 1] == "X" and spots[row + 1][col - 1] == "X"): # check diagonal right Player 1
+                gameWinner = 1
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if (row == col and row == 1 and col == 1 and spots[row][col] == "O" and spots[row - 1][col + 1] == "O" and spots[row + 1][col - 1] == "O"): # check diagonal right Player 1
+                gameWinner = 2
+                print(f"Winner is Player {gameWinner}!")
+                return True
+    return False
+
+def checkDraw(spots):
+    for row in range(3):
+        if all(isinstance(idx, str) for idx in spots[row]) == False: # if any of the indexes are ints, then there are still moves left
+            return False
+    return True
 
 while (not winner):
     val = input(f"Player {player} pick your move: ")
@@ -55,4 +79,8 @@ while (not winner):
         player = 1
     
     if (checkWinner(spots)):
+        winner = True
+
+    if (checkDraw(spots)):
+        print("the game is a Draw!")
         winner = True
