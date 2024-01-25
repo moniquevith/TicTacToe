4fd748ae86ab2d2110115ffdce61dbd06a6419e6
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-25 15:21:50 +1100

    check horizontal and vertical wins

--- main.py
+++ main.py
@@ -9,12 +9,25 @@ winner = False
 gameWinner = 0
 player = 1 # if player 1: X, player 2: O
 
-def checkWinner(spots): 
+def checkWinner(spots):
     for row in range(3):
         for col in range(3):
-            if ((col + 2) == 2 and spots[row][col] == "X" and spots[row][col + 1] == "X" and spots[row][col + 2] == "X"): # check horizontal 
+            if ((col + 2) == 2 and spots[row][col] == "X" and spots[row][col + 1] == "X" and spots[row][col + 2] == "X"): # check horizontal player 1
                 gameWinner = 1
-                winner = True  
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if ((col + 2) == 2 and spots[row][col] == "O" and spots[row][col + 1] == "O" and spots[row][col + 2] == "O"): # check horizontal player 2
+                gameWinner = 2
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if ((row + 2) == 2 and spots[row][col] == "X" and spots[row + 1][col] == "X" and spots[row + 2][col] == "X"): # check vertical player 1
+                gameWinner = 1
+                print(f"Winner is Player {gameWinner}!")
+                return True
+            if ((row + 2) == 2 and spots[row][col] == "O" and spots[row + 1][col] == "O" and spots[row + 2][col] == "O"): # check vertical player 2
+                gameWinner = 2
+                print(f"Winner is Player {gameWinner}!")
+                return True
 
 while (not winner):
     val = input(f"Player {player} pick your move: ")
@@ -41,6 +54,5 @@ while (not winner):
         drawBoard(spots)
         player = 1
     
-    checkWinner(spots)
-
-print(f"Winner is Player {gameWinner}!")
+    if (checkWinner(spots)):
+        winner = True
