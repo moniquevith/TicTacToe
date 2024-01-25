16665ae02956ac8d5a248f8a2589aa54136ba32a
Monique Vith <z5310404@ad.unsw.edu.au>
2024-01-25 15:03:57 +1100

    figured out row and col position of value to ensure players cant choose same spot as other player

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from helper import drawBoard
-
+import math
 print("--------Welcome to Noughts & Crosses ------------- \nCreated by: Monique Vith")
 print("--------------------------------------------------")
 spots = [[1,2,3],[4,5,6],[7,8,9]]
 drawBoard(spots)
 
+winner = False
+gameWinner = 0
+player = 1 # if player 1: X, player 2: O
+
 def checkWinner(spots): 
     for row in range(3):
         for col in range(3):
-            print("to be done")
+            if ((col + 2) == 2 and spots[row][col] == "X" and spots[row][col + 1] == "X" and spots[row][col + 2] == "X"): # check horizontal 
+                gameWinner = 1
+                winner = True  
 
-winner = False
-player = 1 # if player 1: X, player 2: O``
 while (not winner):
     val = input(f"Player {player} pick your move: ")
     if player == 1:
@@ -20,7 +24,7 @@ while (not winner):
                 if (spots[row][col] == int(val)): # if spot isnt taken 
                     spots[row][col] = 'X'
                     break
-                if (spots[row][col] == 'O'): # if spot is taken
+                if (spots[(int(val) - 1)//3][(int(val) - 1) % 3] == 'O'): # if spot is taken
                     val = input("Spot has been taken, choose another move: ")
                     continue
         drawBoard(spots)
@@ -31,10 +35,12 @@ while (not winner):
                 if (spots[row][col] == int(val)): # if spot isnt taken
                     spots[row][col] = 'O'
                     break
-                if (spots[row][col] == 'X'): # if spot is taken
+                if (spots[(int(val) - 1)//3][(int(val) - 1) % 3] == 'X'): # if spot is taken
                     val = input("Spot has been taken, choose another move: ")
                     continue
         drawBoard(spots)
         player = 1
     
     checkWinner(spots)
+
+print(f"Winner is Player {gameWinner}!")
